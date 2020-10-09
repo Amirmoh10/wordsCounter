54b4479bffa9074574d125e907af2a3d72edea2f
Amir <mohamedghezala@gmail.com>
2020-10-10 00:43:50 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [userInput, setUserInput] = React.useState("");
+  let filteredInput;
+  let wordsCount = 0,
+    charCount = 0,
+    charNoSpaceCount = 0;
+  if (userInput.length === 0) {
+    wordsCount = 0;
+  } else {
+    let delimeter = /\s+/gi;
+    filteredInput = userInput
+      .trim()
+      .split(delimeter)
+      .filter((word) => word.length !== 0);
+    wordsCount = filteredInput.length;
+    charCount = userInput.trim().length;
+    charNoSpaceCount = userInput.replace(delimeter, "").length;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="title_1">
+          <p>Word Count</p>
+        </div>
+        <div className="sub-title">
+          <p>A simple and free word counter</p>
+        </div>
+      </div>
+      <div className="countersInputContainer">
+        <div className="counters">
+          <div className="words">
+            <p>{wordsCount}</p>
+            <p>Words</p>
+          </div>
+
+          <div className="characters">
+            <p>{charCount}</p>
+            <p>Characters</p>
+          </div>
+
+          <div className="charExSpaces">
+            <p>{charNoSpaceCount}</p>
+            <p>Charecters Excluding Spaces</p>
+          </div>
+        </div>
+
+        <div className="input-container">
+          <textarea
+            className="input"
+            value={userInput}
+            type="text"
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
